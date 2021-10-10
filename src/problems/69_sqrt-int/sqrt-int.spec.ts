baf53957f798adcdbe16205d2c2d29628a36addc
Ola NguyenVan <olanguyenvan@gmail.com>
2021-10-10 18:14:40 +0200

    Problem 69: Add non recursive solution

--- src/problems/69_sqrt-int/sqrt-int.spec.ts
+++ src/problems/69_sqrt-int/sqrt-int.spec.ts
@@ -1,17 +1,31 @@
-const { sqrt_int } = require("./sqrt-int");
+const { sqrt_int_recursive, sqrt_int_iteration } = require("./sqrt-int");
 
-describe("sqrt_int", () => {
-    it.each([
-        [4, 2],
-        [2, 1],
-        [9, 3],
-        [1, 1],
-        [10, 3],
-        [100, 10],
-        [8, 2],
-        [7, 2],
-        [0, 0],
-    ])("sqrt_int(%s) should be %s", (input, output) => {
-        expect(sqrt_int(input)).toEqual(output);
-    });
+const testCases = [
+    [4, 2],
+    [2, 1],
+    [9, 3],
+    [1, 1],
+    [10, 3],
+    [100, 10],
+    [8, 2],
+    [7, 2],
+    [0, 0],
+];
+
+describe("sqrt_int_recursive", () => {
+    it.each(testCases)(
+        "sqrt_int_recursive(%s) should be %s",
+        (input, output) => {
+            expect(sqrt_int_recursive(input)).toEqual(output);
+        }
+    );
+});
+
+describe("sqrt_int_iteration", () => {
+    it.each(testCases)(
+        "sqrt_int_iteration(%s) should be %s",
+        (input, output) => {
+            expect(sqrt_int_iteration(input)).toEqual(output);
+        }
+    );
 });
--- src/problems/69_sqrt-int/sqrt-int.ts
+++ src/problems/69_sqrt-int/sqrt-int.ts
@@ -1,4 +1,4 @@
-export function sqrt_int(x: number): number {
+export function sqrt_int_recursive(x: number): number {
     return sqrt_int_rec(0, x);
 
     function sqrt_int_rec(a: number, b: number): number {
@@ -18,4 +18,32 @@ export function sqrt_int(x: number): number {
             return sqrt_int_rec(middle, b);
         }
     }
+}
+
+export function sqrt_int_iteration(x: number): number {
+    let a = 0;
+    let b = x;
+    let middle, res;
+    let a_integer_part = Math.trunc(a);
+    let b_integer_part = Math.trunc(b);
+
+    while (a_integer_part !== b_integer_part) {
+        a_integer_part = Math.trunc(a);
+        b_integer_part = Math.trunc(b);
+
+        if (a_integer_part === b_integer_part) {
+            return b_integer_part;
+        }
+
+        middle = (a + b) / 2;
+        res = middle * middle;
+
+        if (res > x) {
+            b = middle;
+        } else {
+            a = middle;
+        }
+    }
+
+    return a_integer_part;
 }
