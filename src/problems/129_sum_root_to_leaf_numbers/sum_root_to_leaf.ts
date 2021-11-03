e1c09432784a4e687fc6b0166aa34a474cff0cb9
Ola Nguyen Van <olanguyenvan@gmail.com>
2021-11-03 16:29:52 +0100

    Problem 129: Code cleanup and Optimization

--- src/problems/129_sum_root_to_leaf_numbers/sum_root_to_leaf.ts
+++ src/problems/129_sum_root_to_leaf_numbers/sum_root_to_leaf.ts
@@ -17,44 +17,51 @@ export function sumNumbers(root: TreeNode | null): number {
     return sumNumbersRec(root)[0];
 
     function sumNumbersRec(node: TreeNode): [number, number[]] {
-        let tmpArray: number[];
-
-        let sum = 0;
+        const currentDigit = node.val;
 
         if (!node.left && !node.right) {
-            tmpArray = [1];
+            // current node is leaf
+            const leavesByLevel = [1];
+            return [currentDigit, leavesByLevel];
+        }
+
+        let sum = 0;
+        let leavesFromLeft: number[] = [];
+        let leavesFromRight: number[] = [];
+
+        if (node.left) {
+            const leftResult = sumNumbersRec(node.left);
+            sum += leftResult[0];
+            leavesFromLeft = leftResult[1];
+        }
+        if (node.right) {
+            const rightResult = sumNumbersRec(node.right);
+            sum += rightResult[0];
+            leavesFromRight = rightResult[1];
+        }
+
+        // [0, 0, 1] means there is one leaf separated by chain of 2 nodes
+        // currentNode -> next -> next -> leaf
+        let leavesByLevel: number[];
+
+        if (leavesFromRight.length > leavesFromLeft.length) {
+            leavesByLevel = leavesFromRight;
+            for (let i = 0; i < leavesFromLeft.length; i++) {
+                leavesByLevel[i] += leavesFromLeft[i];
+            }
         } else {
-            let leftLeaves: number[] = [];
-            let rightLeaves: number[] = [];
-
-            if (node.left) {
-                const leftResult = sumNumbersRec(node.left);
-                sum += leftResult[0];
-                leftLeaves = leftResult[1];
-            }
-            if (node.right) {
-                const rightResult = sumNumbersRec(node.right);
-                sum += rightResult[0];
-                rightLeaves = rightResult[1];
-            }
-
-            tmpArray = new Array(
-                Math.max(rightLeaves.length, leftLeaves.length) + 1
-            ).fill(0);
-
-            for (let i = 0; i < rightLeaves.length; i++) {
-                tmpArray[i + 1] += rightLeaves[i];
-            }
-            for (let i = 0; i < leftLeaves.length; i++) {
-                tmpArray[i + 1] += leftLeaves[i];
+            leavesByLevel = leavesFromLeft;
+            for (let i = 0; i < leavesFromRight.length; i++) {
+                leavesByLevel[i] += leavesFromRight[i];
             }
         }
 
-        // sum the tmp array
-        const currentDigit = node.val;
-        for (let i = 0; i < tmpArray.length; i++) {
-            sum += currentDigit * tmpArray[i] * Math.pow(10, i);
+        // current node is not a leaf, therefore there's no leaf on level 0
+        leavesByLevel.unshift(0);
+
+        for (let i = 0; i < leavesByLevel.length; i++) {
+            sum += currentDigit * leavesByLevel[i] * Math.pow(10, i);
         }
-        return [sum, tmpArray];
+        return [sum, leavesByLevel];
     }
 }
